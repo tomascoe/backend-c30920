403dc33f8360c2607c130d222a3638e3e8f284f6
Tomas Coelho <tomascoe.wd@gmail.com>
2022-03-24 18:57:10 -0600

    Servidor

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const Container = require("./main");
+const cont = new Container("./productos.txt");
+//REQUESTS
+
+app.get("/", (req, res) => res.send("HOME"));
+
+app.get("/productos", (req, res) => {
+  cont.getAll().then((resp) => res.send(resp));
+});
+
+app.get("/productoRandom", (req, res) => {
+  cont
+    .getAll()
+    .then((resp) => res.send(resp[Math.floor(Math.random() * resp.length)]));
+});
+
+//CONFIG
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${server.address().port}`);
+});
+
+server.on("error", (error) => console.log(`Error en el servidor: ${error}`));
